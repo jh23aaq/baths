e1d0cfaa5e071c8227387216e42fac58ea15a476
lf21aaj <lf21aaj@herts.ac.uk>
2025-04-02 10:42:03 +0100

    Uncommented saveGame and loadGame
    lf21aaj - Uncommented saveGame() and loadGame() else if statements in GameUI

--- baths-students git/src/wars/GameUI.java
+++ baths-students git/src/wars/GameUI.java
@@ -73,18 +73,18 @@ public class GameUI
             {
                 System.out.println(myBattles.toString());
             }
-//            else if (choice == 9) // Task 7 only
-//            {
-//                System.out.println("Write to file");
-//                myBattles.saveGame("olenka.dat");
-//            }
-//            else if (choice == 10) // Task 7 only
-//            {
-//                System.out.println("Recommission from file");
-//                SeaBattles myBattles2=null;
-//                myBattles2.loadGame("olenka.dat");
-//                System.out.println(myBattles2.toString());               
-//            }  
+            else if (choice == 9) // Task 7 only
+            {
+                System.out.println("Write to file");
+                myBattles.saveGame("olenka.dat");
+            }
+            else if (choice == 10) // Task 7 only
+            {
+                System.out.println("Recommission from file");
+                SeaBattles myBattles2=null;
+                myBattles2.loadGame("olenka.dat");
+                System.out.println(myBattles2.toString());               
+            }  
         }
         System.out.println("Thank-you");
     }
